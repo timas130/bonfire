f7f235cccb2e7c90fe23e7fe88f9dde1a5f29f3f
sit <me@sit.sh>
2024-08-02 17:10:58 +0300

    feat: filters in PollingResultsScreen

--- CampfireSDK/src/main/java/com/sayzen/campfiresdk/compose/publication/post/pages/polling/PollingResultsScreen.kt
+++ CampfireSDK/src/main/java/com/sayzen/campfiresdk/compose/publication/post/pages/polling/PollingResultsScreen.kt
@@ -1,11 +1,23 @@
 package com.sayzen.campfiresdk.compose.publication.post.pages.polling
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastFilter
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dzen.campfire.api.models.publications.post.PagePolling
 import com.dzen.campfire.api.requests.post.RPostPagePollingGetVotes
@@ -19,6 +31,9 @@ import com.sayzen.campfiresdk.screens.account.profile.SProfile
 import com.sayzen.campfiresdk.support.ApiRequestsSupporter.sendSuspendExt
 import com.sup.dev.android.libs.screens.navigator.Navigator
 import com.sup.dev.java.tools.ToolsDate
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 typealias PollResult = RPostPagePollingGetVotes.PollingResultsItem
 
@@ -29,6 +44,23 @@ class PollingResultsModel(
     init {
         reload()
     }
+
+    private val _pollItemFilter = MutableStateFlow<Long?>(null)
+    val pollItemFilter = _pollItemFilter.asStateFlow()
+
+    fun setFilter(itemId: Long?) {
+        _pollItemFilter.value = itemId
+    }
+
+    // omg this is so clean
+    override val items: StateFlow<List<PollResult>?>
+        get() = super.items.combineStates(pollItemFilter) { items, filter ->
+            if (filter != null) {
+                items?.fastFilter { it.itemId == filter }
+            } else {
+                items
+            }
+        }
 
     override suspend fun load(current: List<PollResult>): List<PollResult> {
         return RPostPagePollingGetVotes(
@@ -63,6 +95,41 @@ private fun PollingResultsScreenC(
         },
         emptyCard = {
             EmptyCard(text = stringResource(R.string.poll_results_empty))
+        },
+        topContent = {
+            val pollItemFilter by model.pollItemFilter.collectAsState()
+
+            LazyRow(
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                itemsIndexed(page.options) { itemId, option ->
+                    FilterChip(
+                        selected = pollItemFilter == itemId.toLong(),
+                        onClick = {
+                            if (pollItemFilter == itemId.toLong()) {
+                                model.setFilter(null)
+                            } else {
+                                model.setFilter(itemId.toLong())
+                            }
+                        },
+                        leadingIcon = {
+                            Row {
+                                AnimatedVisibility(visible = pollItemFilter == itemId.toLong()) {
+                                    Icon(
+                                        imageVector = Icons.Default.Check,
+                                        contentDescription = null,
+                                        modifier = Modifier.size(FilterChipDefaults.IconSize)
+                                    )
+                                }
+                            }
+                        },
+                        label = {
+                            Text(option, maxLines = 1)
+                        }
+                    )
+                }
+            }
         },
         item = { item, shimmer ->
             ListItem(
