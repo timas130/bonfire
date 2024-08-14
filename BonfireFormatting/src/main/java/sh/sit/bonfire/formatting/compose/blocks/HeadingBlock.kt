008f017b4091345fb2de06042fc7e849f94ed39c
sit <me@sit.sh>
2024-08-14 14:50:59 +0300

    refactor: slightly adjust heading sizes 5, 6

--- BonfireFormatting/src/main/java/sh/sit/bonfire/formatting/compose/blocks/HeadingBlock.kt
+++ BonfireFormatting/src/main/java/sh/sit/bonfire/formatting/compose/blocks/HeadingBlock.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.sp
 import sh.sit.bonfire.formatting.compose.LinksClickableText
 import sh.sit.bonfire.formatting.core.model.spans.HeadingBlock
 
@@ -23,8 +24,8 @@ internal fun HeadingBlock(
             2 -> typography.headlineMedium
             3 -> typography.headlineSmall
             4 -> typography.titleLarge
-            5 -> typography.titleMedium
-            6 -> typography.titleSmall
+            5 -> typography.titleMedium.copy(fontSize = 19.sp)
+            6 -> typography.titleSmall.copy(fontSize = 11.sp)
             else -> typography.titleLarge
         }.merge(
             textAlign = LocalTextStyle.current.textAlign,
